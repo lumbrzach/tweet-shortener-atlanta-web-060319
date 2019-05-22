2fc229b152423977fb18b3d5145cf7eaa10b47a1
Zach Parker <parkerz410@gmail.com>
2019-05-22 23:04:49 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -44,7 +44,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   shortened_tweet = selective_tweet_shortener(tweet)
-  
+
   if shortened_tweet.length > 140
     extra_shortened_tweet = "#{shortened_tweet[0..138]} + "...""
     #binding.pry
