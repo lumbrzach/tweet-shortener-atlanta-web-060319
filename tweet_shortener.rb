674d66c6db6ce244f69b766e32db2d48dd29db45
Zach Parker <parkerz410@gmail.com>
2019-05-22 22:20:40 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -45,7 +45,6 @@ end
 def shortened_tweet_truncator(tweet)
   shortened_tweet = selective_tweet_shortener(tweet)
   if shortened_tweet.length > 140
-    shortened_tweet[138..140] = "..."
-    return shortened_tweet  
+    shortened_tweet[138..140] = "..." 
   end
 end
